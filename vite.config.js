169973a137ca61924feca13d18ae9a43870ed2ab
BrunoWavrzenczak <brunowavrzenczak@gmail.com>
2026-05-06 18:26:17 -0300

    update site title to BrunoW and configure base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Adicione a linha abaixo. 
-  // O './' garante que o site encontre os arquivos em qualquer subpasta (como no GitHub Pages)
-  base: './',
+  // Se o './' não funcionou, vamos usar o nome exato do seu repositório.
+  // Vi no seu terminal que a pasta se chama 'meu_site'. 
+  // Se o nome do seu repositório no GitHub for diferente, substitua 'meu_site' abaixo.
+  base: '/meu_site/',
 })
